18eb5f9699de513130f1794069b149a2cf978f82
MehediMehad <132126469+MehediMehad@users.noreply.github.com>
2025-04-13 18:06:18 +0600

    2️⃣ Create a "customers" table with the following fields:

--- solution.sql
+++ solution.sql
@@ -1,6 +1,14 @@
 -- 1️⃣ Find books that are out of stock.
 SELECT * FROM books WHERE stock = 0;
 
+-- 2️⃣ Retrieve the most expensive book in the store.
+SELECT *
+FROM books
+WHERE
+    price = (
+        SELECT MAX(price)
+        FROM books
+    );
 
 
 
