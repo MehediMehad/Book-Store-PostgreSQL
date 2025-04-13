d8826fd20e6b2d11579eadcc1051d06296bde2aa
MehediMehad <132126469+MehediMehad@users.noreply.github.com>
2025-04-13 18:51:57 +0600

    -- 3️⃣ Find the total number of orders placed by each customer.

--- solution.sql
+++ solution.sql
@@ -10,7 +10,17 @@ WHERE
         FROM books
     );
 
+-- 3️⃣ Find the total number of orders placed by each customer.
+SELECT
+    c.name AS customer_name,
+    COUNT(o.id) AS total_orders
+FROM
+    customers c
+LEFT JOIN
+    orders o ON c.id = o.customer_id
+GROUP BY
+    c.name;
 
 
 
-SELECT * FROM books;
+SELECT * FROM orders;
